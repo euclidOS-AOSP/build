9ce4baf73471f9fc7a5b0242d6031813c642cdfa
Daniel Micay <danielmicay@gmail.com>
2021-10-24 23:02:07 -0400

    product: Enable gestural navigation overlay to match default nav mode
    This fixes an occasional issue where the navbar is in a broken state
    (gesture nav works, but the bar is rendered as if it's 2/3-button nav)
    after wiping data or provisioning new users.
    
    From Danny Lin <danny@kdrag0n.dev> in ProtonAOSP.
    
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -21,7 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.android.dataroaming?=true
+    ro.com.android.dataroaming?=true \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
 # More AOSP packages
 PRODUCT_PACKAGES += \
