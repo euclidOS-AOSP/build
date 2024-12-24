ae052f4ad2dc5fb4f791171546bd57056f7017c8
clarencelol <clarencekuiek@proton.me>
2024-12-24 13:25:31 +0800

    build: Do not build AOSP sounds
    * They overrided pixel sounds in GMS
    
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -18,16 +18,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
-# Default AOSP sounds
-ifeq ($(EUCLID_BUILD),)
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
-    ro.config.notification_sound?=pixiedust.ogg \
-    ro.com.android.dataroaming?=true \
+    ro.com.android.dataroaming?=true
 
 # More AOSP packages
 PRODUCT_PACKAGES += \
