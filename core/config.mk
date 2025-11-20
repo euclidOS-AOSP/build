Lunaris-AOSP/build_make

build: Adapt key dir
Change-Id: Ifbde11ea81ebec353bca868c665cfb69b1c0b0d0

core/config.mk
@@ -1353,7 +1353,7 @@ endif
 # when the target-files is signed in a post-build step.
 ifeq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
 BUILD_KEYS := test-keys
-else ifneq ($(filter vendor/lineage-priv/%,$(DEFAULT_SYSTEM_DEV_CERTIFICATE)),)
+else ifneq ($(filter vendor/lunaris-priv/%,$(DEFAULT_SYSTEM_DEV_CERTIFICATE)),)
 BUILD_KEYS := release-keys
 else
 BUILD_KEYS := dev-keys

euclidOS-AOSP/build

Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

core/config.mk
@@ -1353,7 +1353,7 @@ endif
 # when the target-files is signed in a post-build step.
 ifeq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
 BUILD_KEYS := test-keys
-else ifneq ($(filter vendor/lineage-priv/%,$(DEFAULT_SYSTEM_DEV_CERTIFICATE)),)
+else ifneq ($(filter vendor/euclid-priv/%,$(DEFAULT_SYSTEM_DEV_CERTIFICATE)),)
 BUILD_KEYS := release-keys
 else
 BUILD_KEYS := dev-keys